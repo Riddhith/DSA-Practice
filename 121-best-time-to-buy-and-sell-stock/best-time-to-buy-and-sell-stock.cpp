518f5babd85343482e559776247530e76c11d445
Riddhith Banerjee <riddhith04@gmail.com>
2024-02-23 12:03:30 +0530

    Time: 82 ms (84.98%) | Memory: 95.8 MB (20.42%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,8 +4,7 @@ public:
         int max_profit=0;
         int mini=prices[0];
         for(int i=0;i<prices.size();i++){
-            int profit=prices[i]-mini;
-            max_profit=max(profit,max_profit);
+            max_profit=max(prices[i]-mini,max_profit);
             mini=min(mini,prices[i]);
         }
         return max_profit;
