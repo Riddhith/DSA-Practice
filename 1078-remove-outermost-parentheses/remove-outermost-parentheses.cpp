a1f6d4679758aaad4cd25e6d14fdcc1bdb4697c3
Riddhith Banerjee <riddhith04@gmail.com>
2024-03-24 13:29:14 +0530

    Time: 6 ms (18.06%) | Memory: 11.1 MB (11.17%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        vector<string> v;
+        int p=0;
+        string st="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(')
+            p++;
+            else if(s[i]==')')
+            p--;
+            st+=s[i];
+
+            if(p==0){
+                v.push_back(st);
+                st="";
+            }
+        }
+        for(int i=0;i<v.size();i++){
+            string sr="";
+            for(int j=1;j<v[i].length()-1;j++){
+                sr=sr+v[i][j];
+            }
+            v[i]=sr;
+        }
+        string ans="";
+        for(int i=0;i<v.size();i++){
+            ans+=v[i];
+        }
+        return ans;
+    }
+};
