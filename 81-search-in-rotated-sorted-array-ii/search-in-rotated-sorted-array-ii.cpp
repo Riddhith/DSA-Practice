c4f3e43804dfa2aaaba479812d805ae970369a6b
Riddhith Banerjee <riddhith04@gmail.com>
2024-03-28 15:54:57 +0530

    Time: 3 ms (79.66%) | Memory: 16.3 MB (95.39%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -3,7 +3,6 @@ public:
     bool search(vector<int>& nums, int target) {
         int s=0;
         int e=nums.size()-1;
-        int ans=INT_MAX;
         while(s<=e){
             int mid=s+(e-s)/2;
             if(nums[mid]==target){
