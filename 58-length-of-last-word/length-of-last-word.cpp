2420eff1728940c087bd311582ec1ed557c6b2b0
Riddhith Banerjee <riddhith04@gmail.com>
2024-04-01 09:36:12 +0530

    Time: 4 ms (47.87%) | Memory: 7.6 MB (87.89%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -3,11 +3,11 @@ public:
     int lengthOfLastWord(string s) {
         int i=s.length()-1;
         int len=0;
-        while(s[i]==' '){
-            i-=1;
+        while(i>=0 && s[i]==' '){
+            i--;
         }
         while(i>=0 && s[i]!=' '){
-            len=len+1;
+            len++;
             i--;
         }
         return len;
