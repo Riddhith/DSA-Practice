46a82e14218310661a84fe4d5d81557b59f77778
Riddhith Banerjee <riddhith04@gmail.com>
2024-04-05 10:52:22 +0530

    Time: 8 ms (35.76%) | Memory: 9.4 MB (14.62%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char,char> mp;
+        vector<char> v;
+        if(s.length()!=t.length()) return false;
+        for(int i=0;i<s.length();i++){
+            if(mp.find(s[i])==mp.end()){
+                if(find(v.begin(),v.end(),t[i])==v.end()){
+                    mp[s[i]]=t[i];
+                    v.push_back(t[i]);
+                }
+                else return false;
+            }
+            else if(mp[s[i]]!=t[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
