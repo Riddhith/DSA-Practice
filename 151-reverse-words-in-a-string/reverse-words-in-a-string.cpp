d003b03960a0e0e53b4a6cce3fc7ec7339395e31
Riddhith Banerjee <riddhith04@gmail.com>
2024-06-24 11:41:25 +0530

    Time: 165 ms (6.08%) | Memory: 522.4 MB (5.26%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -3,24 +3,31 @@ public:
     string reverseWords(string s) {
         vector<string> v;
         string st="";
-        
         string w="";
+        string w1="";
         for(int i=0;i<s.length();i++){
             if(w.empty() && s[i]==' '){
                 continue;
             }
             w=w+s[i];
         }
-        w=" "+w;
-        for(int i=w.length()-1;i>=0;i--){
-            if(w[i]==' ' && w[i+1]==' ') continue;
-            if(w[i]==' '){
+        reverse(w.begin(),w.end());
+        for(int i=0;i<w.length();i++){
+            if(w1.empty() && w[i]==' '){
+                continue;
+            }
+            w1=w1+w[i];
+        }
+        reverse(w1.begin(),w1.end());
+        w1=" "+w1;
+        for(int i=w1.length()-1;i>=0;i--){
+            if(w1[i]==' ' && w1[i+1]==' ') continue;
+            if(w1[i]==' '){
                 v.push_back(st);
                 st="";
                 continue;
             }
-            
-            st=w[i]+st;
+            st=w1[i]+st;
         }
         string ans="";
         int size=v.size();
@@ -28,13 +35,6 @@ public:
             ans+=v[i]+" ";
         }
         ans+=v[size-1];
-        string w1="";
-        for(int i=0;i<ans.length();i++){
-            if(w1.empty() && ans[i]==' '){
-                continue;
-            }
-            w1=w1+ans[i];
-        }
-        return w1;
+        return ans;
     }
 };
