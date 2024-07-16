1829108aa6d9d474bcc0d95b39ff867db92f9244
Riddhith Banerjee <riddhith04@gmail.com>
2024-07-16 11:00:06 +0530

    Time: 3 ms (58.00%) | Memory: 10.5 MB (5.65%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int l=s.size(),sign=0,c=0;
+        long num=0;
+        string revs="",revss="";
+        bool flag=true;
+        for(int i=0;i<l;i++){
+            if(s[i]==' ' && flag)
+            {
+                continue;
+            }else{
+                flag=false;
+                revs=revs+s[i];
+            }
+        }
+        if(revs[0]=='-'){
+            sign=-1;
+        }else{
+            sign=1;
+            if(revs[0]!='+'){
+                revs='+'+revs;
+            }
+        }
+        for(int i=1;i<revs.length();i++){
+            revss=revss+revs[i];
+        }
+        for(int i=0;i<revss.length();i++){
+            if(revss[i]=='0' && c==0){
+                continue;
+            }else if(revss[i]>='0' && revss[i]<='9'){
+                num=num*10+(revss[i]-'0');
+                if((num*sign)>INT_MAX){
+                    return INT_MAX;
+                }else if((num*sign)<INT_MIN){
+                    return INT_MIN;
+                }
+                c++;
+            }else break;
+        }
+        return num*sign;
+    }
+};
