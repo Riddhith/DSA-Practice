f209c25c4a3bc5bd4667880ed664753c1c8a1369
Riddhith Banerjee <riddhith04@gmail.com>
2024-07-23 19:48:27 +0530

    Time: 158 ms (60.70%) | Memory: 32 MB (43.70%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        int ans=0;
+        for(auto it:mp){
+            while(it.second>=3){
+                it.second = it.second-2;
+            }
+            ans+=it.second;
+            
+        }
+        return ans;
+    }
+};
