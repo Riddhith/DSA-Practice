164f62db3a9417cc6cdc5627fca03c9590b4a73c
Riddhith Banerjee <riddhith04@gmail.com>
2024-08-31 20:50:50 +0530

    Time: 148 ms (36.87%) | Memory: 74.9 MB (38.07%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,8 +10,56 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if (fast->next != NULL)
+            return slow->next;
+        return slow;
+    }
+    ListNode* sortTwoLists(ListNode* first, ListNode* second) {
+        // Write your code here.
+        ListNode* dummyhead = new ListNode(-1);
+        ListNode* third = dummyhead;
+        ListNode* temp1 = first;
+        ListNode* temp2 = second;
+        while (temp1 && temp2) {
+            if (temp1->val <= temp2->val) {
+                third->next = temp1;
+                third = third->next;
+                temp1 = temp1->next;
+            } else {
+                third->next = temp2;
+                third = third->next;
+                temp2 = temp2->next;
+            }
+        }
+        if (temp1) {
+            third->next = temp1;
+        } else {
+            third->next = temp2;
+        }
+        return dummyhead->next;
+    }
+    ListNode* mS(ListNode* head){
         if(head==NULL || head->next==NULL) return head;
+        ListNode* middle=middleNode(head);
+        ListNode* righthead=middle->next;
+        ListNode* lefthead=head;
+        middle->next=NULL;
+        lefthead=mS(lefthead);
+        righthead=mS(righthead);
+        return sortTwoLists(lefthead,righthead);
+    }
+    ListNode* sortList(ListNode* head) {
+        head=mS(head);
+        return head;
+        // brute
+        /*if(head==NULL || head->next==NULL) return head;
         ListNode* temp=head;
         vector<int> v;
         while(temp){
@@ -26,6 +74,6 @@ public:
             i++;
             temp=temp->next;
         }
-        return head;
+        return head;*/
     }
 };
