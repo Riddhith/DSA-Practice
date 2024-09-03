43ad0999576e889b447bb5f62737bc02bd84ad47
Riddhith Banerjee <riddhith04@gmail.com>
2024-09-03 20:03:48 +0530

    Time: 3 ms (85.62%) | Memory: 16.7 MB (38.30%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // recursive
+        if (head == NULL || head->next == NULL)
+            return head;
+        ListNode* newHead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL) return head;
+        int c=0;
+        ListNode* temp=head;
+        while(temp){
+            c++;
+            temp=temp->next;
+        }
+        cout<<c<<endl;
+        k=k%c;
+        // cout<<k<<endl;
+        if(k==0 || k==c) return head;
+        int p=c-k;
+        int i=0;
+        temp=head;
+        while(temp){
+            i++;
+            if(i==p) break;
+            temp=temp->next;
+        }
+        ListNode* secondhead=temp->next;
+        temp->next=NULL;
+        ListNode* firsthead=head;
+        ListNode* move=secondhead;
+        while(move->next!=NULL){
+            move=move->next;
+        }
+        move->next=head;
+        return secondhead;
+    }
+};
