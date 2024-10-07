effbcebee97102c17c9d5b176554fc56495452b0
Riddhith Banerjee <riddhith04@gmail.com>
2024-10-07 21:48:44 +0530

    Time: 538 ms (8.28%) | Memory: 11.8 MB (78.27%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -4,18 +4,18 @@ public:
         int c=0;
         vector<bool> prime(n+1,true);
         prime[0]=prime[1]=0;
-        for(int i=2;i*i<=n;i++){
-            if(prime[i]){
-                // c++;
-                for(int j=i*i;j<n;j=j+i)
-                prime[j]=0;
-            }
-        }
         for(int i=2;i<n;i++){
             if(prime[i]){
                 c++;
+                for(int j=i*2;j<n;j=j+i)
+                prime[j]=0;
             }
         }
+        // for(int i=2;i<n;i++){
+        //     if(prime[i]){
+        //         c++;
+        //     }
+        // }
         return c;
     }
 };
