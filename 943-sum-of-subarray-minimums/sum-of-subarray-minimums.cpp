4ed52e63ca12d29387694a65752e31f3d06c7313
Riddhith Banerjee <riddhith04@gmail.com>
2024-11-24 11:11:42 +0530

    Time: 1203 ms (5.14%) | Memory: 50.2 MB (5.35%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> findPsee(vector<int>& arr,int n){
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i]){
+                st.pop();
+            }
+            ans[i]=st.empty()?-1:st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> findNse(vector<int> arr,int n){
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            ans[i]=st.empty()?(n):st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> nse(n);
+        vector<int> pse(n);
+        nse=findNse(arr,n);
+        pse=findPsee(arr,n);
+        for(int i=0;i<nse.size();i++){
+            cout<<nse[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<pse.size();i++){
+            cout<<pse[i]<<" ";
+        }
+        long long total=0;
+        int mod=(int)(1e9+7);
+        for(int i=0;i<arr.size();i++){
+            long long right=nse[i]-i;
+            long long left=i-pse[i];
+            total=(total+(right*left*arr[i])%mod)%mod;
+        }
+        return total;
+    }
+};
