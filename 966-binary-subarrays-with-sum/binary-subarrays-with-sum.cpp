faa00d01c027ce98d4d8ef4cdbb13163cfbe3cb4
Riddhith Banerjee <riddhith04@gmail.com>
2024-12-18 15:36:16 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (46.22%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
+  int lessequaltok(vector<int>& nums,int goal){
+        if(goal < 0)
+        return 0;
+
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+        int n = nums.size();
+        int sum = 0;
+
+        while(r < n){
+        sum += nums[r];
+
+        while(sum > goal){
+            sum -= nums[l];
+            l++;
+        }
+        ans += (r-l+1);
+        r++;
+        }
+        return ans;
+        
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int c=0;
+        /*int c=0;
         int n=nums.size();
         vector<int> prefix_sum(n,0);
         prefix_sum[0]=nums[0];
@@ -18,6 +41,7 @@ public:
             }
             mp[prefix_sum[i]]++;
         }
-        return c;
+        return c;*/
+        return lessequaltok(nums,goal)-lessequaltok(nums,goal-1);
     }
 };
