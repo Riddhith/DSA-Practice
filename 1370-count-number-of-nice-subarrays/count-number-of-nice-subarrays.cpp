838065898d9fd790d3529e0894aff220c86d0e09
Riddhith Banerjee <riddhith04@gmail.com>
2024-12-20 13:35:58 +0530

    Time: 17 ms (28.67%) | Memory: 71.5 MB (48.50%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -7,9 +7,9 @@ public:
         int ans=0;
         int sum=0;
         while(r<n){
-            sum+=temp[r];
+            sum+=(temp[r]%2);
             while(sum>k){
-                sum-=temp[l];
+                sum-=(temp[l]%2);
                 l++;
             }
             ans+=(r-l+1);
@@ -18,15 +18,15 @@ public:
         return ans;
     }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int> temp;
-        for(int i=0;i<n;i++){
-            if(nums[i]%2!=0){
-                temp.push_back(1);
-            }else{
-                temp.push_back(0);
-            }
-        }
-        return lessequaltok(temp,k)-lessequaltok(temp,k-1);
+        // int n=nums.size();
+        // vector<int> temp;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]%2!=0){
+        //         temp.push_back(1);
+        //     }else{
+        //         temp.push_back(0);
+        //     }
+        // }
+        return lessequaltok(nums,k)-lessequaltok(nums,k-1);
     }
 };
