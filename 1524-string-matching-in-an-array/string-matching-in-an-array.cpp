0ad348f3a3cb6432f6bfa80ee53c0ea3dda1e41a
Riddhith Banerjee <riddhith04@gmail.com>
2025-01-07 23:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (60.52%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<string> stringMatching(vector<string>& words) {
         int n=words.size();
         vector<string> ans;
-        for(int i=0;i<n;i++){
+        /*for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i==j) continue;
                 int a=words[i].size();
@@ -22,6 +22,17 @@ public:
                 if(flag==true){
                     ans.push_back(words[i]);
                     break;
+                }
+            }
+        }*/
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                    int f=words[j].find(words[i]);
+                    if(f != string::npos){
+                        ans.push_back(words[i]);
+                        break;
+                    }
                 }
             }
         }
