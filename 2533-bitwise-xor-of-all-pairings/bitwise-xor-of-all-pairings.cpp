57070db967ec4101490c86f505bf85f7254d404e
Riddhith Banerjee <riddhith04@gmail.com>
2025-01-16 10:27:17 +0530

    Time: 0 ms (100.00%) | Memory: 64.1 MB (82.93%) - LeetSync

---
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size();
+        int n=nums2.size();
+        int ans=0;
+        if(m%2!=0){
+            for(auto i:nums2)
+            ans^=i;
+        }
+        if(n%2!=0){
+            for(auto i:nums1){
+                ans^=i;
+            }
+        }
+        return ans;
+    }
+};
