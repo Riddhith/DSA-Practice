18d604b3d83383b2346eb959484d004c7e16879f
Riddhith Banerjee <riddhith04@gmail.com>
2025-01-17 20:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (50.83%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> left(n, 0);
+        int n = ratings.size(),sum=1;
+        /*vector<int> left(n, 0);
         vector<int> right(n, 0);
         left[0] = 1;
         for (int i = 1; i < n; i++) {
@@ -23,6 +23,30 @@ public:
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += max(left[i], right[i]);
+        }*/
+        int i=1;
+        while(i<n) {
+            if(ratings[i]==ratings[i-1]) {
+                sum+=1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]) {
+                peak+=1;
+                i++;
+                sum+=peak;
+            }
+            int down=0;
+            while(i<n && ratings[i]<ratings[i-1]) {
+                down+=1;
+                i++;
+                sum+=down;
+            }
+            down++;
+            if(down>peak) {
+                sum+=down-peak;
+            }
         }
         return sum;
     }
